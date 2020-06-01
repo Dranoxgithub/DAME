920f2a6e933bc0d18d4d25c3895974daeb550407
Anni Chen <anni.chen@duke.edu>
2020-05-31 22:45:09 -0400

    Remove for loops

--- Desktop/2020summer/casual-inference/DAME/GenerateNewActiveSets.R
+++ Desktop/2020summer/casual-inference/DAME/GenerateNewActiveSets.R
@@ -1,8 +1,9 @@
-# all ordered such as no group like {2,1}1
-# s is a newly dropped set, delta is the set of previously processed sets 
-# s is a vector, 
+# all ordered such as no group like {2,1}
+# s is a newly dropped set, delta is the set of previously processed sets
+# s is a vector
 GenerateNewActiveSets <- function(s, delta) {
-  # get size k 
+  s <- sort(s)
+  # get size k
   k <- length(s)
   
   # initialize the return set Z
@@ -13,51 +14,34 @@ GenerateNewActiveSets <- function(s, delta) {
   length_list <- lapply(delta, length)
   subset_index <- which(length_list == k)
   delta_k <- list()
-  for (i in 1:length(subset_index)) {
-    delta_k[[i]] <- delta[[subset_index[i]]]
-  }
+
+  delta_k <- lapply(1:length(subset_index), function(i) {delta_k[[i]] <- delta[[subset_index[i]]]})
   delta_k[[length(delta_k) + 1]] <- s
   
   # step 4 get all covariates contained in stes in delta_k
   cov_delta_k <- delta_k[[1]]
-  for (i in 1:length(delta_k)) {
-    cov_delta_k <- union(cov_delta_k, delta_k[[i]])
-  }
+  cov_delta_k <- lapply(1:length(delta_k), function(x) {union(cov_delta_k, delta_k[[x]])})[[length(delta_k)]]
   cov_delta_k <- sort(cov_delta_k)
   
   # step 5 find the support of covariate e in delta_k
-  S_set <- data.frame(cov_delta_k = cov_delta_k)
-  S_set['support'] <- 0
-  for (i in 1:length(delta_k)) {
-    current_set <- delta_k[[i]]
-    intersection <- intersect(current_set, cov_delta_k)
-    if (length(intersection) == 0) {
-      next
-    }
-    for (k in 1:length(intersection)) {
-      intersect_number <- intersection[k]
-      S_set[which(cov_delta_k == intersect_number), 2] <- S_set[which(S_set$cov_delta_k == intersect_number), 2] + 1
-    }
-  }
+  merge_delta_k <- unlist(delta_k)
+  S_set <- plyr::count(merge_delta_k)
   
-  # step 5 and 6: find the covariates in cov_ have enough support in delta_k, including s first
-  candidates <- S_set[which(S_set$support >= k),1]
-  check_s <- intersect(candidates, s)
-  if (length(check_s) == length(s)) {
+  # step 5 and 6: find the covariates in cov_ have enough support in delta_ks , including s first
+  candidates <- S_set[which(S_set[2] >= k), 1]
+  if (all(s %in% candidates)) {
     # exclude s
-    new_cov_candidates_true <- lapply(candidates, function(x) {x %in% s})
-    new_cov_candidates <- which(new_cov_candidates_true == FALSE)
-    for (i in 1:length(new_cov_candidates)) {
-      current_set <- c(s, new_cov_candidates[i])
-      current_set <- sort(current_set)
+    new_cov_candidates <- setdiff(candidates, s)
+    for (current_new_cov in new_cov_candidates) {
+      current_set <- sort(c(s, current_new_cov))
       subset <- list()
       subset <- lapply(1:(k+1), function(x) current_set[-c(x)])
       check_subset <- lapply(1:(k+1), function(x) list(subset[[x]]) %in% delta_k) # set is in reversed order
       if (length(which(check_subset == TRUE)) == k + 1) {
         Z[[length(Z) + 1]] <- current_set
       }
-      
     }
   }
   return (Z)
 }
+
